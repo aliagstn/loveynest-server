c02c8440e88c9d9d0fbacb907ead99cc69fd09a0
sahmoen <sahmguban28@gmail.com>
2022-07-17 21:16:53 +0700

    Feat: Finish App Quiz

--- controllers/appQuizController.js
+++ controllers/appQuizController.js
@@ -7,6 +7,7 @@ class AppQuizControl {
       const quizList = await AppQuiz.findAll();
 
       // console.log(quizList[0].question);
+      console.log(quizList[0]);
       res.status(200).json({
         statusCode: 200,
         quizList,
@@ -26,10 +27,52 @@ class AppQuizControl {
       let newResult = { responseUser, QuizId, UserId, coupleId };
       const resultQuiz = await AppQuizResult.create(newResult);
 
-      console.log(resultQuiz);
+      // console.log(resultQuiz);
       res.status(201).json({
         statusCode: 201,
         resultQuiz,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "ISA",
+      });
+    }
+  }
+
+  static async getResult(req, res, next) {
+    try {
+      const resultList = await AppQuizResult.findAll({
+        include: [Couple],
+        order: [["id", "ASC"]],
+      });
+
+      res.status(200).json({
+        statusCode: 200,
+        resultList,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "ISA",
+      });
+    }
+  }
+
+  static async getResultByUser(req, res, next) {
+    try {
+      const userId = +req.params.id;
+      const resultByIdList = await AppQuizResult.findAll({
+        where: {
+          UserId: userId,
+        },
+        order: [["id", "DESC"]],
+        limit: 7,
+      });
+
+      res.status(200).json({
+        statusCode: 200,
+        resultByIdList,
       });
     } catch (err) {
       console.log(err);
--- routers/app-quiz.js
+++ routers/app-quiz.js
@@ -3,7 +3,10 @@ const express = require("express");
 const routerApp = express.Router();
 const AppQuizControl = require("../controllers/appQuizController");
 
-routerApp.get("/getQuiz", AppQuizControl.getAppQuiz);
-routerApp.post("/createQuiz", AppQuizControl.createResult);
+routerApp.get("/quiz", AppQuizControl.getAppQuiz);
+routerApp.get("/results", AppQuizControl.getResult);
+routerApp.post("/results", AppQuizControl.createResult);
+
+routerApp.get("/results/:id", AppQuizControl.getResultByUser);
 
 module.exports = routerApp;
