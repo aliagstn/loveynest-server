1a81fb37f555484ff81c796e7fe1d3d1897a4cdb
lordRedsky <aminmuhammadrizki@gmail.com>
2022-07-17 08:22:46 +0800

    update user quizzes 02 feat: create question feat: get total point create user quiz documentation update error handler update router

--- routers/quiz-router.js
+++ routers/quiz-router.js
@@ -3,10 +3,11 @@ const QuizController = require('../controllers/quiz-controller')
 
 router.get('/quizes', QuizController.getQuiz)
 router.post('/quiz', QuizController.createQuiz)
-// router.post('/quiz-categories', QuizController.createQuiz)
+// router.get('/quiz/categories', QuizController.createQuiz)
 router.patch('/quiz/:quizId/:questionId', QuizController.updateResponseQuiz)
 router.get('/quiz/:quizId', QuizController.getQuizById)
-// router.get('/total-score', QuizController.totalScore)
+router.post('/quiz/:quizId', QuizController.createQuestion)
+router.get('/quiz/:quizId/total-score', QuizController.totalScore)
 
 module.exports = router
 
